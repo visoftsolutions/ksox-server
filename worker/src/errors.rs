4c7c3d3638f4a3c412eae9c59ecb93e0e2e4348d
Paweł <neotheprogramist@proton.me>
2023-09-10 21:47:12 +0200

    cargo clippy

--- worker/src/errors.rs
+++ worker/src/errors.rs
@@ -1,14 +1,14 @@
 #[derive(Debug, thiserror::Error)]
 pub enum WorkerError {
     #[error("axum server error")]
-    HyperError(#[from] hyper::Error),
+    Hyper(#[from] hyper::Error),
 
     #[error("environment variable error")]
-    EnvVarError(#[from] std::env::VarError),
+    EnvVar(#[from] std::env::VarError),
 
     #[error("parse address error")]
-    AddressParseError(#[from] std::net::AddrParseError),
+    AddressParse(#[from] std::net::AddrParseError),
 
     #[error("tracing setup error")]
-    TracingError(#[from] tracing::subscriber::SetGlobalDefaultError),
+    Tracing(#[from] tracing::subscriber::SetGlobalDefaultError),
 }
